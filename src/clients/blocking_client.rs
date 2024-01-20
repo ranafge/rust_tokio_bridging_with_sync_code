011dcd98a49c6d4f452765f4c6f5af210073406b
ranafge <ranafge@gmail.com>
2024-01-21 01:31:20 +0600

    using block_on

--- src/clients/blocking_client.rs
+++ src/clients/blocking_client.rs
@@ -5,7 +5,7 @@ pub use tokio_topics_bridging_with_sync_code::clients::client::Message;
 
 // Established connection with a Redis server.
 // structure synchronous interface to mini-redis
-pub struct BlockingCient {
+pub struct BlockingClient {
     /// The asyncronous `Client` from mini-redis
     inner: tokio_topics_bridging_with_sync_code::clients::Client,
     /// A current_thread runtime for executing operations on the 
@@ -14,7 +14,7 @@ pub struct BlockingCient {
     // Runtime fomr Tokio responsible for executing asynchronous operation
 }
 
-impl BlockingCient {
+impl BlockingClient {
     // connect method is instace of BlockingClient T is implement ToSocketAddrs that converted to a socket address
     pub fn connect<T: ToSocketAddrs>(addr: T) -> tokio_topics_bridging_with_sync_code::Result<BlockingCient> {
         let rt = tokio::runtime::Builder::new_multi_thread()
@@ -24,4 +24,84 @@ impl BlockingCient {
         let inner = rt.block_on(tokio_topics_bridging_with_sync_code::clients::Client::connect(addr))?;
         Ok(BlockingCient {inner, rt})
     }
+
+    pub fn get(&mut self, key:&str) -> tokio_topics_bridging_with_sync_code::Result<Option<Bytes>> {
+        self.rt.block_on(self.inner.get(key))
+    }
+
+    pub fn set(&mut self, key: &str, value: Bytes) -> tokio_topics_bridging_with_sync_code::Result<()> {
+        self.rt.block_on(self.inner.set(key, value))
+    }
+
+    pub fn set_expires (
+        &mut self,
+        key: &str,
+        value: Bytes,
+        expiration: Duration,
+    )-> tokio_topics_bridging_with_sync_code::Result<()> {
+        self.rt.block_on(self.inner.set_expires(key, value, expiration))
+    }
+        
+    pub fn publish(&mut self, channel: &str, message: Bytes) -> tokio_topics_bridging_with_sync_code::Result<u64> {
+        self.rt.block_on(self.inner.publish(channel, message))
+    }
+
+    pub fn subscribe(self, channels: Vec<String>) -> tokio_topics_bridging_with_sync_code::Result<BlockingSubscriber>{
+        let subscriber = self.rt.block_on(self.inner.subscribe(channel))?;
+        Ok(BlockingSubscriber {
+            inner: subscriber,
+            rt: self.rt
+        })
+    }
+
 }
+
+
+
+pub struct BlockingSubscriber {
+    inner: tokio_topics_bridging_with_sync_code::clients::Subscriver,
+    rt: Runtime
+}
+
+
+
+impl BlockingSubscriber {
+    pub fn get_subscribed(&self) -> &[String] {
+        self.inner.get_subscribed()
+    }
+    pub fn next_message(&mut self) -> tokio_topics_bridging_with_sync_code::Result<Option<Message>> {
+        self.rt.block_on(self.inner.next_message())
+    }
+    pub fn subscribe(&mut self, channels: &[String]) -> crate::Result<()> {
+        self.rt.block_on(self.inner.subscribe(channels))
+    }
+
+    pub fn unsubscribe(&mut self, channels: &[String]) -> crate::Result<()> {
+        self.rt.block_on(self.inner.unsubscribe(channels))
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 
 
-#[tokio::main] // start at run time project run in main asyncronously.
-async fn main() {
-    println!("Hello, world!");
-}
+// #[tokio::main] // start at run time project run in main asyncronously.
+// async fn main() {
+//     println!("Hello, world!");
+// }
 
 
 // async fn main turn into this below
@@ -20,3 +20,38 @@ fn main() {
 }
 
  */
+
+ // Other approch to imopl the above code
+ use tokio::runtime::Builder;
+ use tokio::time::{Duration, sleep};
+
+ fn main() {
+     let runtime = Builder::new_multi_thread()
+     .worker_threads(1)
+     .enable_all()
+     .build()
+     .unwrap();
+
+    let mut handles = Vec::with_capacity(10);
+
+    for i in 0..10 {
+        // spwan return a joinhandle that is a future store in vector handles
+        handles.push(runtime.spawn(my_bg_task(i)));
+    }
+
+    std::thread::sleep(Duration::from_millis(10));
+    println!("Finished time-consuming task.");
+
+    for handle in handles {
+        // block_on wait for all handle to complete
+        runtime.block_on(handle).unwrap();
+    }
+ }
+
+ async fn my_bg_task(i: u64) {
+    let miliis = 1000 - 50 * i;
+    println!("Task {} sleeping for {} ms", i, miliis);
+    sleep(Duration::from_millis(miliis)).await;
+    println!("Task {} stopping.", i);
+
+ }
